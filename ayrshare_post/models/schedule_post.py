e8170d059216d50bfb7820b9e21d83d623b6f1c6
Gianmichele Siano <gio@DESKTOP-HJ6635V>
2023-05-19 14:25:27 +0200

    alline rep

--- ayrshare_post/models/schedule_post.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class schedule_post(models.Model):
-    _name = 'schedule.post'
-    _description = 'Schedule Post'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-
--- google_my_busniness/models/models.py
+++ google_my_busniness/models/models.py
@@ -10,9 +10,41 @@ class google_my_busniness(models.Model):
     displayName = fields.Char(string='Display Name')
     transale_comment = fields.Text(string='Translated Comment')
     orignal_comment = fields.Text(string='Original Comment')
-    createTime = fields.Datetime(string='Created On', default=fields.Datetime.now)
-    updateTime = fields.Datetime(string='Last Updated On', default=fields.Datetime.now)
+    createTime = fields.Text(string='Created On')
+    updateTime = fields.Text(string='Last Updated On')
     name = fields.Char(string='Name')
     google_my_business_id = fields.Char(string='Google My Business ID')
 
 
+    @api.model_create_multi
+    def create(self, vals):
+        res = super().create(vals)
+        print(res)
+        if res:
+            for item in res:
+                reply_vals = {
+                     #'google_business_id': res.id,
+                    'displayName': item.displayName,
+                    'transale_comment': item.transale_comment,
+                    'orignal_comment': item.orignal_comment,
+                    'createTime': item.createTime,
+                    'updateTime': item.updateTime,
+                    'google_my_business_id': item.google_my_business_id,
+                    # copiare le informazioni necessarie dal record `GoogleBusiness` al record `GoogleBusinessReply`
+                }
+                self.env['google_my_busniness.google_my_busniness_reply'].create(reply_vals)
+        return res
+
+
+
+class google_my_busniness_reply(models.Model):
+    _name = 'google_my_busniness.google_my_busniness_reply'
+    _description = 'Google My Business replay'
+
+    displayName = fields.Char(string='Display Name')
+    transale_comment = fields.Text(string='Translated Comment')
+    orignal_comment = fields.Text(string='Original Comment')
+    createTime = fields.Text(string='Created On')
+    updateTime = fields.Text(string='Last Updated On')
+    name = fields.Char(string='Name')
+    google_my_business_id = fields.Char(string='Google My Business ID')
